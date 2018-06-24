713a4e85c4dbf9127b7fc02f14325ee51d6ff37f
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-24 11:16:26 +0500

    Modified API.

--- connection.mongoose.js
+++ connection.mongoose.js
@@ -1,5 +1,18 @@
+/*
+-- mLab --
+DATABASE NAME: mn-mynotesapp
+Database Username: emad
+User Password: emadmlab123
+
+*/
+
 //Dependencies
 var mongoose = require('mongoose');
 
 //Connecting to MongoDB
-mongoose.connect('mongodb://localhost/MyNotesApp');
+
+//mongoose.connect('mongodb://localhost/MyNotesApp');
+mongoose.connect('mongodb://emad:emadmlab123@ds217131.mlab.com:17131/mn-mynotesapp', () => 
+    {
+        console.log("Connected to DB...");
+    });
